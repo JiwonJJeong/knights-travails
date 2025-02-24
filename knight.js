dc51d9e649b3aebeda1dc09278d8b01b8bd657d0
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-24 14:59:56 -0800

    Initial commit

---
+++ knight.js
@@ -0,0 +1,13 @@
+// chess board is 7 x 7
+// each vertices is (x,y) on the board (49 vertices)
+
+// finds (one of the) shortest path between start and end
+// prints number of moves and the path taken
+const knightMoves = function(start, end){
+
+}
+
+// represent vertex and its connections
+const vertex = function(){
+    
+}
